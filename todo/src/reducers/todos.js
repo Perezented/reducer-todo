2dcf4a82af58c22355fe7b697b5a42796dc0dbd0
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-13 15:35:06 -0500

    Changed up the reducers file name to todos.js. I was close when I did it originally, but it was giving me errors. The Docs page has something similar to what I had, must've missed a comma/semicolon.

---
+++ todo/src/reducers/todos.js
@@ -0,0 +1,43 @@
+//starting off with...
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589,
+};
+
+//reducer function
+// export function reducer(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_TODO':
+//             return { ...state, {
+//                 id: action.id,
+//                     text: action.text,
+//                         completed: false,
+//             } };
+//         case 'REMOVE_TODO':
+//             return { ...state.remove(removeTodo) };
+//         default:
+//             return initialState;
+//     }
+// }
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false,
+                },
+            ];
+        case 'TOGGLE_TODO':
+            return state.map((todo) =>
+                todo.id === action.id
+                    ? { ...todo, completed: !todo.completed }
+                    : todo
+            );
+        default:
+            return state;
+    }
+};
