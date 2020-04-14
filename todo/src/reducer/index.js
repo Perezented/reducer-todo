abe492d6b812d5d503f61ddcaa913d074167b024
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-13 20:32:49 -0500

    did another npm create-react-app. Added some of the files and spotted where I could improve on the first go.

---
+++ todo/src/reducer/index.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589,
+};
+export const reducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false,
+                },
+            ];
+        case 'TOGGLE_TODO':
+            return state.map((todo) =>
+                todo.id === action.id
+                    ? { ...todo, completed: !todo.completed }
+                    : todo
+            );
+        default:
+            return state;
+    }
+};
